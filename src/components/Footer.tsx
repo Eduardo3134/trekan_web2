282d2d3cf058b9e0405f5fc026a7b4a7b9635215
Arturo <avildosola@trekan.co>
2026-05-19 21:36:02 -0400

    Change text in whatsapp contact icon

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -45,7 +45,7 @@ const socialLinks = [
   {
     icon: <WhatsAppIcon />,
     label: "WhatsApp",
-    href: "https://wa.me/56965231694?text=Hola%2C%20quiero%20probar%20la%20Demo",
+    href: "https://wa.me/56965231694?text=Hola",
     bg: "#25D366",
   },
 ];
